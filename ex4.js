bf45865314440ffe3533111873f7e61f878f4f3b
ArijKouki <86109233+ArijKouki@users.noreply.github.com>
2022-04-17 17:33:43 +0100

    Add files via upload

---
+++ ex4.js
@@ -0,0 +1,12 @@
+const getRandomColor =()=>
+"#"+Math.floor(Math.random()*16777215).toString(16)
+
+
+const clickColorElement=(element)=>{
+    element.addEventListener(
+        "click",
+        (e)=>{e.target.style.color= getRandomColor()}
+    )
+}
+
+clickColorElement(document.querySelector("ol"));
